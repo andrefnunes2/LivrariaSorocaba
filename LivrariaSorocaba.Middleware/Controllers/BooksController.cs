247e2866e1ed117585215e2e2083520453f34a9f
André <andre.nunes@ventron.com.br>
2018-04-24 13:25:18 -0300

    fix web.http

--- LivrariaSorocaba.Middleware/Controllers/BooksController.cs
+++ LivrariaSorocaba.Middleware/Controllers/BooksController.cs
@@ -1,12 +1,6 @@
 ﻿using LivrariaSorocaba.Entities;
 using System;
-using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
-using System.Reflection;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
 
 namespace LivrariaSorocaba.Middleware.Controllers
 {
--- LivrariaSorocaba/Controllers/MainController.cs
+++ LivrariaSorocaba/Controllers/MainController.cs
@@ -1,9 +1,5 @@
 ﻿using LivrariaSorocaba.Entities;
 using LivrariaSorocaba.Middleware.Controllers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace LivrariaSorocaba.Controllers
